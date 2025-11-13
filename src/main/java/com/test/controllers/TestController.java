73b1130a76e70c20b68ee27a8c8a80d23493a65e
Vats0528 <vatosoaranadison@gmail.com>
2025-11-13 09:59:59 +0300

    sprint 4 et 4 bis

--- src/main/java/com/test/controllers/TestController.java
+++ src/main/java/com/test/controllers/TestController.java
@@ -3,23 +3,12 @@ package com.test.controllers;
 import com.framework.annotation.*;
 
 
-<<<<<<< Updated upstream
-    @Url("/home") // Devient /front/home
-    public String home() {
-        return "Bienvenue sur la page d'accueil 🏠 - Controller: " + this.getClass().getSimpleName();
-    }
-
-    @Url("/about") // Devient /front/about
-    public String about() {
-        return "À propos du framework 🚀 - Controller: " + this.getClass().getSimpleName();
-=======
 @Controller
 public class TestController {
 
     @Url("/test")
     public String hello() {
         return "Bonjour depuis TestController.hello()";
->>>>>>> Stashed changes
     }
 
     @Url("/time")
